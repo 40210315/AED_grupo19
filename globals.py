70e66f7dbd32f382783653071b6d17fc62200792
40210315 <92998384+40210315@users.noreply.github.com>
2023-01-23 10:18:46 +0000

    Update globals.py

--- globals.py
+++ globals.py
@@ -1,4 +1,4 @@
 global verifiedUser
 
 # verifiedUser = ["userType", "userName"]
-verifiedUser = ["admin", "admin1"]
+verifiedUser = ["admin", "admin1"] # Dados de Controlo, deve ser verifiedUser = ["", ""] por predefinição
